2872327044fd68f58496ec444120eeb06708365a
mansouriyassine <mansouri.yassine1@gmail.com>
2024-02-01 12:09:19 -0800

    VERSION YASSINE

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """First endpoint (route) will be to return the status of the API"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
+from api.v1.views import app_views
 
 app = Flask(__name__)
-
-
-from api.v1.views import app_views
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Custom 404 error handler returning JSON"""
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,12 +2,12 @@
 """ Initialize the views """
 
 from flask import Blueprint
-from .index import *
-from .states import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from .index import * 
 from .cities import *
 from .users import *
 from .places_reviews import *
 from .places_amenities import *
 from .places import *
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
