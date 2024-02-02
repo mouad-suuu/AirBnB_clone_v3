647becebe505e750c7f2a2fb2d29dd23da7deb7c
mouad-suuu <moad.menniui@gmail.com>
2024-02-02 18:29:24 +0100

    task 7 state.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,10 @@
-#!/usr/bin/python3
-""" Initialize the views """
-
 from flask import Blueprint
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from .index import * 
-from .cities import *
-from .users import *
-from .places_reviews import *
-from .places_amenities import *
-from .places import *
+from api.v1.views.index import *  # noqa
+from api.v1.views.states import *  # noqa
+from api.v1.views.cities import *  # noqa
+from api.v1.views.amenities import *  # noqa
+from api.v1.views.users import *  # noqa
+from api.v1.views.places import *  # noqa
+from api.v1.views.places_reviews import *  # noqa
+from api.v1.views.places_amenities import *  # noqa
