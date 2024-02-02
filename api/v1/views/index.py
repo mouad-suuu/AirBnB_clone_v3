d3c86aef4d48219bf276dbb21e0f419514242c3e
mouad-suuu <moad.menniui@gmail.com>
2024-02-02 17:44:36 +0100

    task 5 the index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,19 @@
 """ Index """
 
 from flask import jsonify
-# Import the blueprint from the local package context
-from . import app_views
+from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    return jsonify({"status": "OK"})
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieve the number of each object type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
