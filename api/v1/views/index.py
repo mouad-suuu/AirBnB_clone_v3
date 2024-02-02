91f41aea6a1531cddd9a163792813899e4167eb8
mouad-suuu <moad.menniui@gmail.com>
2024-02-02 18:15:19 +0100

    task 5 the index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,62 +1,11 @@
 #!/usr/bin/python3
-"""
-Index model holds the endpoint (route)
-"""
-from api.v1.views import app_views, storage
-from flask import jsonify
+""" Index """
+
+from flask import jsonify , Flask
+# Import the blueprint from the local package context
+from . import app_views
 
 
-@app_views.route('/status/')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Example endpoint returns status
-    returns the current status of the API
-    ---
-    definitions:
-      status:
-        type: object
-      Color:
-        type: string
-      items:
-        $ref: '#/definitions/Color'
-
-    responses:
-      200:
-        description: dictionary with 'status' as key and 'ok' as keyvalue
-        schema:
-          $ref: '#/definitions/State'
-        examples:
-            {"status": "OK"}
-    """
     return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats/')
-def stats():
-    """Example endpoint returns stats
-    returns a number of objects of each class
-    ---
-    definitions:
-      status:
-        type: object
-      Color:
-        type: string
-      items:
-        $ref: '#/definitions/Color'
-
-    responses:
-      200:
-        description: dictionary with 'status' as key and 'ok' as keyvalue
-        schema:
-          $ref: '#/definitions/State'
-        examples:
-           { "amenities": 47, "cities": 36, "places": 154, "reviews": 718,
-             "states": 27, "users": 31}
-    """
-    models_available = {"User": "users",
-                        "Amenity": "amenities", "City": "cities",
-                        "Place": "places", "Review": "reviews",
-                        "State": "states"}
-    stats = {}
-    for cls in models_available.keys():
-        stats[models_available[cls]] = storage.count(cls)
-    return jsonify(stats)
