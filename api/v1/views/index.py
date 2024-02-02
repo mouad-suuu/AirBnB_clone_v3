24f13f0bee8e0ae624443ed7d964aee75adbd218
mouad-suuu <moad.menniui@gmail.com>
2024-02-02 19:23:30 +0100

    updating

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """ Index """
-
 from flask import jsonify
-# Import the blueprint from the local package context
+from models import storage
 from . import app_views
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieves the number of each object by type."""
+    obj_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(obj_counts)
